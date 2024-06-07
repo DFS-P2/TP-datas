fdc4183ecfecf0956fd69a7253b12f3ade1acac7
webew <fillion.bertrand@gmail.com>
2024-06-07 11:52:37 +0200

    localstorage

--- js/index.js
+++ js/index.js
@@ -3,7 +3,14 @@ import feedTbody from "./modules/feedTbody.js";
 
 const usersDatasUrl = "./datas/users.json";
 
-const users = await getDatas(usersDatasUrl);
+let users = [];
+
+if (localStorage.getItem("users")) {
+	users = JSON.parse(localStorage.getItem("users"));
+} else {
+	users = await getDatas(usersDatasUrl);
+	localStorage.setItem("users", JSON.stringify(users));
+}
 
 if (users) {
 	feedTbody(users);
