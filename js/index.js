1e7c2e2b566bf6805275f95b98a53b6e2716783c
webew <fillion.bertrand@gmail.com>
2024-06-07 16:45:33 +0200

    sort by nom

--- js/index.js
+++ js/index.js
@@ -14,26 +14,32 @@ if (localStorage.getItem("users")) {
 }
 
 if (users) {
+	const flechesNom = document.querySelectorAll(".fleche");
+	flechesNom.forEach((fleche) => {
+		fleche.addEventListener("click", () => {
+			console.log(fleche.dataset.direction); // "asc" ou "desc"
+			sortByNom(fleche.dataset.direction);
+			feedTbody(users);
+		});
+	});
+	console.log(flechesNom);
+	function sortByNom(direction) {
+		console.log(users);
+		let sortDirection;
+		if (direction == "asc") {
+			sortDirection = 1;
+		} else if (direction == "desc") {
+			sortDirection = -1;
+		}
+		users.sort((a, b) => {
+			const x = a.nom.toLowerCase();
+			const y = b.nom.toLowerCase();
+			if (x < y) return -1 * sortDirection;
+			if (x > y) return 1 * sortDirection;
+			return 0;
+		});
+	}
+
 	feedTbody(users);
 	handleSearch(users);
 }
-
-// const tab = [
-// 	{
-// 		id: 1,
-// 		nom: "Turing",
-// 		prenom: "Alan",
-// 	},
-// 	{
-// 		id: 2,
-// 		nom: "Lovelace",
-// 		prenom: "Ada",
-// 	},
-// ];
-// const searchValue = "lan";
-// const filteredTab = tab.filter(
-// 	(element) =>
-// 		element.nom.toLowerCase().includes(searchValue) ||
-// 		element.prenom.toLowerCase().includes(searchValue)
-// );
-// console.log(filteredTab);
